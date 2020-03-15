29a2c8e2a0e6aaac88be81d7c1309046087d1b28
Dewha <scherbinin96@gmail.com>
2020-03-15 17:41:26 +0500

    Добавил index.php и style.css
    сделал шапку и подвал

---
+++ www/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang='ru'>
+<head>
+	<!--кодировка-->
+	<meta charset="UTF-8">
+	<!--размер страницы под устройства-->
+	<meta name="viewport" content="width=device-width",initial-scale=1.0">
+	<!--для браузера edge и ie-->
+	<meta http-equiv="X-UA-Compatitable content="ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Online Tester</title>
+</head>
+<body>
+  <!--header-->
+	<div class="header">
+		<h1 class="logo">Online Tester</h1>
+		<nav class="header-right">
+			<a class="text-dark" href="#">Главная</a>
+			<a class="text-dark" href="#">Возможности</a>
+			<a class="text-dark" href="#">Авторы</a>
+		</nav>
+			<a class="btn" href="#">Войти</a>
+	</div>
+
+	<!--Footer-->
+	<footer class="text-muted">
+	  <div class="footer-text">
+		<p class="footer-right">
+		  <a class="text-dark" href="#top">Наверх</a>
+		</p>
+		<p>Система онлайн-тестирования Online Tester</p>
+		<p>Лф ПНИПУ ЭВТ-17-1б 2020г.</p>
+	  </div>
+	</footer>
+</body>
+</html>
