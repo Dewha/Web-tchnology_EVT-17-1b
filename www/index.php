0baf7eaf0d997a7ee0b54ba6818726a3d19d577d
Dewha <scherbinin96@gmail.com>
2020-03-15 21:48:12 +0500

    добавил шаблон для заполнения главной страницы

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@
 	<title>Online Tester</title>
 </head>
 <body>
-  <!--header-->
+	<!--header-->
 	<div class="header">
 		<h1 class="logo">Online Tester</h1>
 		<nav class="header-right">
@@ -21,6 +21,44 @@
 		</nav>
 			<a class="btn" href="#">Войти</a>
 	</div>
+	<!--Banner-->
+
+	<!--Main-->
+	<div class="mainpage">
+		<h3 class="text-center">Как это работает?</h3>
+		<div class="row">
+			<div class="col">
+				<img src="img/test.png" class="num">
+				<div class="main-text">
+					<h4>Шаг 1</h4>
+					<div>какой-то текст</div>
+				</div>
+			</div>
+			<div class="col">
+				<img src="img/test.png" class="num">
+				<div class="main-text">
+					<h4>Шаг 2</h4>
+					<div>какой-то текст</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<img src="img/test.png" class="num">
+				<div class="main-text">
+					<h4>Шаг 3</h4>
+					<div>какой-то текст</div>
+				</div>
+			</div>
+			<div class="col">
+				<img src="img/test.png" class="num">
+				<div class="main-text">
+					<h4>Шаг 4</h4>
+					<div>какой-то текст</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<!--Footer-->
 	<footer class="text-muted">
@@ -28,8 +66,8 @@
 		<p class="footer-right">
 		  <a class="text-dark" href="#top">Наверх</a>
 		</p>
-		<p>Система онлайн-тестирования Online Tester</p>
-		<p>Лф ПНИПУ ЭВТ-17-1б 2020г.</p>
+		<p>Система онлайн-тестирования Online Tester<br>
+			Лф ПНИПУ ЭВТ-17-1б 2020г.</p>
 	  </div>
 	</footer>
 </body>
