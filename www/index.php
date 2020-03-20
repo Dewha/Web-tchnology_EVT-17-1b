b8e4db82ef7060b99c629a5bd678c90b7862453d
Dewha <scherbinin96@gmail.com>
2020-03-20 21:58:55 +0500

    Добавил баннер и переделал блок информации

--- www/index.php
+++ www/index.php
@@ -11,50 +11,77 @@
 	<title>Online Tester</title>
 </head>
 <body>
+
 	<!--header-->
 	<div class="header">
 		<h1 class="logo">Online Tester</h1>
 		<nav class="header-right">
-			<a class="text-dark" href="#">Главная</a>
+			<a class="text-dark" href="index.html">Главная</a>
 			<a class="text-dark" href="#">Возможности</a>
 			<a class="text-dark" href="#">Авторы</a>
 		</nav>
 			<a class="btn" href="#">Войти</a>
 	</div>
+
 	<!--Banner-->
+	<div class="banner">
+		<h1 class="banner-text-big"> Система тестирования Online Tester </h1>
+		<h2 class="banner-text-small"> Онлайн сервис для проверки знаний</h2>
+	</div>
 
 	<!--Main-->
 	<div class="mainpage">
-		<h3 class="text-center">Как это работает?</h3>
-		<div class="row">
-			<div class="col">
-				<img src="img/test.png" class="num">
-				<div class="main-text">
+		<h3 class="grid-header">Как это работает?</h3>
+		<div class="grid">
+			<div class="grid-content">
+				<img src="C:\Users\Андрей\Desktop\универ\сайт\test.png" class="num">
+				<div class="grid-text">
 					<h4>Шаг 1</h4>
-					<div>какой-то текст</div>
+					<div>Lorem ipsum dolor sit amet,
+						consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation
+						ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit
+						esse cillum dolore eu fugiat nulla pariatur. Excepteur
+						sint occaecat cupidatat non proident, sunt in culpa qui
+						officia deserunt mollit anim id est laborum.</div>
 				</div>
 			</div>
-			<div class="col">
-				<img src="img/test.png" class="num">
-				<div class="main-text">
+			<div class="grid-content">
+				<img src="C:\Users\Андрей\Desktop\универ\сайт\test.png" class="num">
+				<div class="grid-text">
 					<h4>Шаг 2</h4>
-					<div>какой-то текст</div>
+					<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+						deserunt mollit anim id est laborum.</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<img src="img/test.png" class="num">
-				<div class="main-text">
+			<div class="grid-content">
+				<img src="C:\Users\Андрей\Desktop\универ\сайт\test.png" class="num">
+				<div class="grid-text">
 					<h4>Шаг 3</h4>
-					<div>какой-то текст</div>
+					<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+						dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.</div>
 				</div>
 			</div>
-			<div class="col">
-				<img src="img/test.png" class="num">
-				<div class="main-text">
+			<div class="grid-content">
+				<img src="C:\Users\Андрей\Desktop\универ\сайт\test.png" class="num">
+				<div class="grid-text">
 					<h4>Шаг 4</h4>
-					<div>какой-то текст</div>
+					<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+						nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+						in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+						occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 				</div>
 			</div>
 		</div>
